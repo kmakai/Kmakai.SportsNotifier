13946a2ef2007e6e2a2c3e5ce4e39280dc88e382
kmakai <makaikwag@gmail.com>
2023-08-04 18:16:24 -0400

    app completed. must use App passwords on gmail

---
+++ Kmakai.SportsNotifier/Mailer.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Net;
+using System.Net.Mail;
+
+namespace Kmakai.SportsNotifier;
+
+public class Mailer
+{
+    private string Email { get; set; }
+    private string Password { get; set; }
+    private string Subject { get; set; } = "Sports Results";
+
+    public Mailer(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
+
+
+    public async void SendEmail(string toEmail, string body)
+    {
+        var client = new SmtpClient("smtp.gmail.com", 587)
+        {
+            EnableSsl = true,
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential(Email, Password)
+        };
+
+        var message = new MailMessage(Email, toEmail)
+        {
+            Subject = Subject,
+            Body = body
+        };
+
+        await client.SendMailAsync(message);
+    }
+}
